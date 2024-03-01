37bc998f43611d6ff98323790424b9dba975ccfc
xandoalka <xandoalka@gmail.com>
2024-03-01 21:24:21 +0700

    percobaan contact email

--- js/script.js
+++ js/script.js
@@ -48,6 +48,43 @@ window.onscroll = () => {
 
   footer.classList.toggle(
     "show-animate",
-    window.innerHeight + window.scrollY >= document.documentElement.offsetHeight - 100
+    window.innerHeight + window.scrollY >=
+      document.documentElement.offsetHeight - 100
   );
 };
+
+// send email
+
+const form = document.querySelector("form");
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const subject = document.getElementById("subject");
+const mess = document.getElementById("message");
+
+function sendEmail() {
+  const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${email.value}<br> Phone: ${phone.value}<br> Subject: ${subject.value}<br> Message: ${mess.value}`;
+
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "xandoalka@gmail.com",
+    Password: "6CF3DAA1F70AF294FEE0E691DE1D3E04191C",
+    To: "xandoalka@gmail.com",
+    From: "xandoalka@gmail.com",
+    Subject: subject.value,
+    Body: bodyMessage,
+  }).then(message => {
+    if (message == "OK") {
+      Swal.fire({
+        icon: "success",
+        title: "Success!",
+        text: "Message sent successfully!",
+      })
+    }
+  });
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  sendEmail();
+});
